fa01433dcbb6c40ebe8cb447ba91d7715189e838
Gary Mauger <mauger.gary@gmail.com>
2024-10-12 19:00:00 +0200

    sidebar

--- interface.py
+++ interface.py
@@ -87,59 +87,72 @@ logo_image = Image.open(logo_path)
 #st.image(logo_image, width=900)
 
 #st.title("Aide à la décision pour l'octroi d'un crédit")
+st.markdown(
+    "<h1 style='text-align: center; font-size: 3em;'>Aide à la décision pour l'octroi d'un crédit</h1>",
+    unsafe_allow_html=True
+)
 
 
 # 1. Section : Saisie du numéro du client
-clients = get_clients_list()  # Obtenir la liste complète des clients
+# Vérifier si la liste des clients est déjà chargée dans la session
+if "clients" not in st.session_state:
+    st.session_state.clients = get_clients_list()  # Obtenir la liste complète des clients
 
 # Créer une ligne de colonnes pour l'entrée du numéro de client et les informations sur le client
 col1, col2 = st.columns([2, 2])  # Ajustez les proportions si nécessaire
 
 with col1:
+    st.image(logo_image, width=1000)
+    st.write(" ")  # ligne vide pour laisser un espace
+
     client_id_input = st.text_input("Entrez le numéro du client souhaité :")
-    
-    # Afficher le logo en haut de la page
-    st.image(logo_image, width=900)
 
     if st.button("Valider"):
         if client_id_input:
             client_id_input = int(client_id_input)  # Convertir l'input en entier
             # Vérifier si le client existe dans la liste complète
-            if client_id_input in clients:
+            if client_id_input in st.session_state.clients:
                 st.session_state.selected_client = client_id_input
 
                 # Charger les données du client et les informations de crédit
-                st.session_state.client_data = get_client_data(st.session_state.selected_client)
-                st.session_state.credit_info = get_credit_info(st.session_state.selected_client)
-
-                # Afficher les informations sur le client
-                if st.session_state.client_data:
-                    st.subheader(f"Informations sur le client")
-                    with st.expander("Détails du client"):
-                        st.write(st.session_state.client_data)
-
-                # Afficher les informations de crédit
-                if st.session_state.credit_info:
-                    st.subheader("Informations de crédit")
-                    with st.expander("Détails du crédit"):
-                        st.write(st.session_state.credit_info)
+                if "client_data" not in st.session_state or st.session_state.selected_client != client_id_input:
+                    st.session_state.client_data = get_client_data(st.session_state.selected_client)
+                    st.session_state.credit_info = get_credit_info(st.session_state.selected_client)
 
                 # Prédiction du score de crédit
-                st.subheader("Prédiction du score de crédit")
-                prediction, proba = predict_credit(st.session_state.selected_client)
-                if prediction is not None:
-                    proba_defaut = (1 - proba) * 100  # Supposant que proba est la probabilité de non-défaut
-                    st.write(f"Prédiction : **{'Prêt accordé' if prediction == 0 else 'Prêt refusé'}**")
-                    st.write(f"Probabilité de défaut : **{proba_defaut:.2f}%**")
+                if "prediction" not in st.session_state or st.session_state.selected_client != client_id_input:
+                    st.session_state.prediction, st.session_state.proba = predict_credit(st.session_state.selected_client)
+
+                # Afficher la prédiction si disponible
+                if st.session_state.prediction is not None:
+                    # Convertir la probabilité en pourcentage pour le défaut
+                    proba_defaut = (1 - st.session_state.proba) * 100  # Supposant que proba est la probabilité de non-défaut
+                    if st.session_state.prediction == 0:
+                        # Prêt accordé
+                        st.markdown(
+                            f"<div style='background-color: green; padding: 10px; color: white; border-radius: 5px;'>"
+                            f"<strong>Prédiction : Prêt accordé</strong><br>"
+                            f"Probabilité de défaut : {proba_defaut:.2f}%"
+                            f"</div>",
+                            unsafe_allow_html=True
+                        )
+                    else:
+                        # Prêt refusé
+                        st.markdown(
+                            f"<div style='background-color: red; padding: 10px; color: white; border-radius: 5px;'>"
+                            f"<strong>Prédiction : Prêt refusé</strong><br>"
+                            f"Probabilité de défaut : {proba_defaut:.2f}%"
+                            f"</div>",
+                            unsafe_allow_html=True
+                        )
             else:
                 st.error("Le numéro de client saisi n'est pas dans la liste des clients.")
         else:
             st.error("Veuillez entrer un numéro de client.")
 
 with col2:
-    
-    st.title("Aide à la décision pour l'octroi d'un crédit")
-    
+    st.write(" ")  # ligne vide pour laisser un espace
+
     # Afficher les informations sur le client si déjà sélectionné
     if "selected_client" in st.session_state:
         # Afficher les informations sur le client
@@ -156,22 +169,35 @@ with col2:
 
 
 
+
+
 # ---------------------------------------- Description des colonnes ---------------------------------------- #
 
-st.subheader("Liste des colonnes disponibles")
+# Utiliser la sidebar pour afficher la liste des colonnes disponibles
+with st.sidebar:
+    st.subheader("Liste des colonnes disponibles")
 
-# Récupérer la liste de toutes les colonnes
-columns = get_all_features()  # Appeler la nouvelle fonction
+    # Vérifier si la liste des colonnes est déjà chargée dans la session
+    if "columns" not in st.session_state:
+        st.session_state.columns = get_all_features()  # Charger les colonnes une seule fois
 
-# Vérifiez si la liste des colonnes n'est pas vide
-if columns:
-    # Afficher une liste déroulante pour sélectionner une colonne
-    selected_column = st.selectbox("Sélectionnez une colonne :", columns)
+    # Vérifiez si la liste des colonnes n'est pas vide
+    if st.session_state.columns:
+        # Afficher une liste déroulante pour sélectionner une colonne
+        selected_column = st.selectbox("Sélectionnez une colonne :", st.session_state.columns)
 
-    # Afficher la description de la colonne sélectionnée
-    if selected_column:
-        description = get_column_description(selected_column)
-        if description is not None:
-            st.write(f"**Description de la colonne '{selected_column}':** {description}")
-else:
-    st.write("Aucune colonne disponible.")
+        # Stocker la description de la colonne sélectionnée dans la session
+        if "selected_column_description" not in st.session_state:
+            st.session_state.selected_column_description = ""
+
+        if selected_column:
+            # Vérifier si la description est déjà stockée
+            if selected_column != st.session_state.selected_column_description:
+                st.session_state.selected_column_description = get_column_description(selected_column)
+
+            # Afficher la description de la colonne sélectionnée
+            description = st.session_state.selected_column_description
+            if description is not None:
+                st.write(f"**Description de la colonne '{selected_column}':** {description}")
+    else:
+        st.write("Aucune colonne disponible.")
