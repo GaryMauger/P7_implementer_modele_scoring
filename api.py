fe76844ac2548eb46a5f4a603822ae62a533dcc7
Gary Mauger <mauger.gary@gmail.com>
2024-10-18 21:18:46 +0200

    streamlit infos clients et crédit

--- api.py
+++ api.py
@@ -77,23 +77,23 @@ df_clients_scaled.head()
 
 # Définition des seuils avec leurs noms associés du business score
 thresholds = {
-    "sans": {"valeur": 0.05, "nom": "Sans risque"},
-    "faible": {"valeur": 0.17, "nom": "Faible coût"},
-    "modere": {"valeur": 0.50, "nom": "Coût modéré"},
-    "eleve": {"valeur": 0.28, "nom": "Coût élevé"}
+    "Sans": {"valeur": 0.05, "nom": "Sans risque"},
+    "Faible": {"valeur": 0.17, "nom": "Faible coût"},
+    "Modéré": {"valeur": 0.50, "nom": "Coût modéré"},
+    "Elevé": {"valeur": 0.28, "nom": "Coût élevé"}
 }
 
 class SeuilNom(str, Enum):
-    sans = "sans"
-    faible = "faible"
-    modere = "modere"
-    eleve = "eleve"
+    Sans = "Sans"
+    Faible = "Faible"
+    Modéré = "Modéré"
+    Elevé = "Elevé"
 
 class RequestObject(BaseModel):
     client_id: Optional[int] = None
     feat_number: Optional[int] = None
     feat_name: Optional[str] = None
-    seuil_nom: SeuilNom = SeuilNom.faible  # Utilise l'Enum avec une valeur par défaut
+    seuil_nom: SeuilNom = SeuilNom.Faible  # Utilise l'Enum avec une valeur par défaut
 
 # Modèle de réponse
 class ClientResponse(BaseModel):
@@ -121,7 +121,7 @@ def read_root():
 ###################################################       FONCTION DE PRÉDICTION       ######################################################
 ###################################################
 
-def predict(client_id, seuil_nom="faible"):
+def predict(client_id, seuil_nom="Faible"):
     """
     Effectue la prédiction du score de crédit pour un client donné en utilisant le seuil spécifié.
     
@@ -210,7 +210,7 @@ def plot_waterfall(client_id):
 
     # Afficher le graphique waterfall
     plt.figure(figsize=(12, 8))
-    shap.waterfall_plot(shap_values[0])  # Utiliser shap_values pour le client
+    shap.waterfall_plot(shap_values[0], show=False)  # Utiliser shap_values pour le client
     plt.title(f"Graphique Waterfall pour le client {client_id}")
     
     plt.tight_layout()
@@ -226,6 +226,7 @@ def plot_waterfall(client_id):
 
 
 ###################################################       ROUTE POUR LE GRAPHIQUE WATERFALL       ######################################################
+
 @app.get("/waterfall/{client_id}")
 def get_waterfall(client_id: int):
     try:
@@ -234,13 +235,73 @@ def get_waterfall(client_id: int):
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
     
- 
- 
-    
 
+ 
+###################################################
+###################################################       FONCTION POUR LES INFORMATIONS CLIENTS       ######################################################
+###################################################
+    
+###################################################       ROUTE POUR LA LISTE DES CLIENTS       ######################################################
 
 @app.get("/clients")
 def get_clients():
     """Renvoie une liste d'identifiants clients."""
     client_ids = df_clients_scaled.index.tolist()  # Récupérer les IDs clients
     return {"client_ids": client_ids}
+
+
+###################################################       FONCTION POUR DES INFORMATIONS SUR LE CLIENT       ######################################################
+
+def client_info(client_id):
+    """
+    Retourne les informations personnelles du client sélectionné.
+    """
+    client_info_columns = [
+        "CODE_GENDER", "CNT_CHILDREN", "FLAG_OWN_CAR", "FLAG_OWN_REALTY",
+        "NAME_FAMILY_STATUS", "NAME_HOUSING_TYPE", "NAME_EDUCATION_TYPE",
+        "NAME_INCOME_TYPE", "OCCUPATION_TYPE", "AMT_INCOME_TOTAL"
+    ]
+    
+    client_data = df_application_test.loc[df_application_test['SK_ID_CURR'] == client_id, client_info_columns]
+    
+    if client_data.empty:
+        raise HTTPException(status_code=404, detail=f"Aucun client trouvé avec SK_ID_CURR = {client_id}")
+
+    client_info = client_data.T.fillna('N/A')  # Transposer et remplacer les valeurs manquantes par 'N/A'
+    return client_info.to_dict()  # Convertir en dictionnaire pour un format JSON
+
+
+###################################################       ROUTE POUR DES INFORMATIONS SUR LE CLIENT       ######################################################
+
+@app.get("/client_info/{client_id}")
+def get_client_info(client_id: int):
+    """
+    Endpoint pour obtenir les informations d'un client donné.
+    """
+    return client_info(client_id)
+
+
+###################################################       FONCTION POUR DES INFORMATIONS SUR LE CREDIT       ######################################################
+
+def credit_info(client_id):
+    """
+    Retourne les informations de crédit pour le client sélectionné.
+    """
+    credit_info_columns = ["NAME_CONTRACT_TYPE", "AMT_CREDIT", "AMT_ANNUITY", "AMT_GOODS_PRICE"]
+    credit_data = df_application_test.loc[df_application_test['SK_ID_CURR'] == client_id, credit_info_columns]
+
+    if credit_data.empty:
+        raise HTTPException(status_code=404, detail=f"Aucune information de crédit trouvée pour SK_ID_CURR = {client_id}")
+    credit_info = credit_data.T.fillna('N/A')
+    return credit_info.to_dict()  # Transpose et convertit en dictionnaire
+
+
+###################################################       ROUTE POUR DES INFORMATIONS SUR LE CREDIT       ######################################################
+
+@app.get("/credit_info/{client_id}")
+def get_credit_info(client_id: int):
+    """
+    Endpoint pour obtenir les informations de crédit d'un client donné.
+    """
+    return credit_info(client_id)
+
--- app.py
+++ app.py
@@ -35,6 +35,38 @@ def fetch_client_ids():
         st.error("Erreur lors de la récupération des identifiants clients.")
         return []
 
+def get_client_info(client_id):
+    """Récupération des informations du client via l'API."""
+    response = requests.get(f"{API_URL}/client_info/{client_id}")
+    if response.status_code == 200:
+        client_data = response.json()
+        # Récupérer la première clé dynamique
+        first_key = next(iter(client_data))  # Récupérer la première clé du dictionnaire
+        client_info = client_data.get(first_key, None)
+        if client_info is None:
+            st.error(f"Aucune information trouvée pour le client ID {client_id}.")
+        return client_info
+    else:
+        st.error("Erreur lors de la récupération des informations client.")
+        return None
+
+def get_credit_info(client_id):
+    """Récupération des informations de crédit du client via l'API."""
+    response = requests.get(f"{API_URL}/credit_info/{client_id}")
+    if response.status_code == 200:
+        credit_data = response.json()
+        # Récupérer la première clé dynamique
+        first_key = next(iter(credit_data))  # Récupérer la première clé du dictionnaire
+        credit_info = credit_data.get(first_key, None)
+        if credit_info is None:
+            st.error(f"Aucune information de crédit trouvée pour le client ID {client_id}.")
+        return credit_info
+    else:
+        st.error("Erreur lors de la récupération des informations de crédit.")
+        return None
+
+
+
 
 # ---------------------------------------- Interface Streamlit ----------------------------------------
 # Charger le logo depuis le chemin spécifié
@@ -51,13 +83,33 @@ st.title("Prédiction de Scoring Crédit")
 valid_client_ids = fetch_client_ids()
 
 # Saisie de l'identifiant client
-client_id = st.selectbox("Sélectionnez l'ID du client :", valid_client_ids)
+client_id = st.selectbox("Sélectionnez l'ID du client :", valid_client_ids, key="client_id")
 
-# Saisie de l'identifiant client
-#client_id = st.number_input("Entrez l'ID du client :", min_value=0)
+# Dictionnaire pour les seuils
+#seuils = {
+#    "sans": "Seuil très bas (risque élevé de faux positifs)",
+#    "faible": "Minimiser le Coût tout en Conservant une Bonne Précision",
+#    "modere": "Tolérer un Coût Modéré pour Améliorer la Précision",
+#    "eleve": "Réduire le Risque des Faux Négatifs à Tout Prix"
+#}
 
 # Saisir le seuil
-seuil_nom = st.selectbox("Sélectionnez le seuil :", options=["sans", "faible", "modere", "eleve"])
+#seuil_nom = st.selectbox("Sélectionnez le seuil :", options=list(seuils.keys()), format_func=lambda x: seuils[x], key="seuil")
+
+# Dictionnaire pour les seuils
+seuils = {
+    "Sans": "Sans seuil : Classe presque tous les clients comme à risque, entraînant un nombre élevé de faux positifs et des coûts importants. À éviter.",
+    "Faible": "Faible : Bon compromis entre coût et précision (Coût : 30 295 €, Précision : 86 %). Idéal pour équilibrer détection des défauts et rentabilité.",
+    "Modéré": "Modéré : Maximiser la précision (Coût : 36 725 €, Précision : 92 %), mais augmente les faux négatifs. Convient si la réputation est primordiale.",
+    "Elevé": "Élevé : Réduit le risque de faux négatifs (Coût : 101 570 €, Précision : 91 %), adapté pour éviter les pertes financières majeures."
+}
+
+# Saisir le seuil avec une description
+seuil_nom = st.selectbox("Sélectionnez le seuil :", options=list(seuils.keys()), key="seuil")
+
+# Afficher la description du seuil sélectionné
+#st.write("### Description du seuil sélectionné :")
+st.write(seuils[seuil_nom])
 
 if st.button("Prédire"):
     # Appel de la fonction de prédiction
@@ -69,16 +121,44 @@ if st.button("Prédire"):
         st.write(f"**Prédiction :** {'Défaut' if prediction == 1 else 'Pas de défaut'}")
         
         # Afficher les features utilisées
-        st.write("**Features utilisées :**")
-        st.json(features)
+        #st.write("**Features utilisées :**")
+        #st.json(features)
 
         # Récupérer le graphique SHAP
         shap_image = get_waterfall_graph(client_id)
         if shap_image:
             st.image(shap_image, use_column_width=True)
 
+# Récupérer les informations du client pour les afficher dans la sidebar
+client_info = get_client_info(client_id)
+credit_info = get_credit_info(client_id)
 
 with st.sidebar:
     st.image(logo_image, use_column_width=True)
+    st.write("")
+    st.markdown("---")  # Ligne de séparation    
+    st.subheader("Informations sur le client")
+    if client_info:
+        
+        # Utilisation de get() pour éviter les KeyErrors
+        st.write(f"**Genre** : {client_info.get('CODE_GENDER', 'Non renseigné')}")
+        st.write(f"**Nombre d'enfants** : {client_info.get('CNT_CHILDREN', 'Non renseigné')}")
+        st.write(f"**Possède une voiture** : {'Oui' if client_info.get('FLAG_OWN_CAR', 'N') == 'Y' else 'Non'}")
+        st.write(f"**Possède un bien immobilier** : {'Oui' if client_info.get('FLAG_OWN_REALTY', 'N') == 'Y' else 'Non'}")
+        st.write(f"**État civil** : {client_info.get('NAME_FAMILY_STATUS', 'Non renseigné')}")
+        st.write(f"**Type de logement** : {client_info.get('NAME_HOUSING_TYPE', 'Non renseigné')}")
+        st.write(f"**Niveau d'éducation** : {client_info.get('NAME_EDUCATION_TYPE', 'Non renseigné')}")
+        st.write(f"**Type de revenu** : {client_info.get('NAME_INCOME_TYPE', 'Non renseigné')}")
+        st.write(f"**Occupation** : {client_info.get('OCCUPATION_TYPE', 'Non renseigné')}")
+        st.write(f"**Revenu total annuel** : {client_info.get('AMT_INCOME_TOTAL', 0):,.0f} €")
     
-    st.subheader("Informations sur le client et le crédit")
+    # Ajout d'une barre de séparation
+    st.markdown("---")  # Ligne de séparation
+        
+    st.subheader("Informations sur le crédit")
+    if credit_info:
+        st.write(f"**Type de contrat** : {credit_info.get('NAME_CONTRACT_TYPE', 'Non renseigné')}")
+        st.write(f"**Montant du crédit** : {credit_info.get('AMT_CREDIT', 'Non renseigné')}")
+        st.write(f"**Montant de l'annuité** : {credit_info.get('AMT_ANNUITY', 'Non renseigné')}")
+        st.write(f"**Montant des biens** : {credit_info.get('AMT_GOODS_PRICE', 'Non renseigné')}")
+
